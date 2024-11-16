cbf84b9c7e90db4e5eb537e30fac04839810566a
Alma <cemeralma6@gmail.com>
2024-11-16 23:58:34 +0100

    Add: login and logout basics

--- backend-api/Data/AppDbContext.cs
+++ backend-api/Data/AppDbContext.cs
@@ -9,4 +9,5 @@ using ProjectNamespace.Models;
 public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
 {
     public DbSet<Student> Students { get; set; }
+    public DbSet<User> Users { get; set; }
 }
