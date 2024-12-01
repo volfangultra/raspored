0d4468a6e312fc6c87d9404ce10ae62d34f7ad6c
Alma <cemeralma6@gmail.com>
2024-12-01 14:33:17 +0100

    Fixing login page

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,49 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Input, Segment, Form, Grid, Header, Loader } from 'semantic-ui-react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container, Loader } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState(null);
   const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userRole, setUserRole] = useState(null);
 
   useEffect(() => {
-    setUsername('');
-    setPassword('');
     const savedToken = localStorage.getItem('token');
     const savedRole = localStorage.getItem('userRole');
     if (savedToken && savedRole) {
       setToken(savedToken);
+      setUserRole(savedRole);
     }
   }, []);
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ Username: username, PasswordHash: password }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Login failed');
-      }
-
-      const data = await response.json();
-      setToken(data.token);
-
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('userRole', data.role);
-    } catch (error) {
-      console.error('Error logging in:', error);
-    }
-  };
 
   const handleLogout = () => {
     setIsLoggingOut(true);
@@ -53,6 +28,7 @@ function App() {
     setTimeout(() => {
       window.location.reload();
     }, 100);
+    window.location.pathname = '/';
   };
 
   if (isLoggingOut) {
@@ -76,54 +52,32 @@ function App() {
 
   return (
     <Container style={{ marginTop: '20px' }}>
-      {token ? (
         <Router>
-          <Layout onLogout={handleLogout}>
+          {token ? (
+            <Layout onLogout={handleLogout}>
+              <Routes>
+                {userRole === 'user' &&
+                  <>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/raspored" element={<MainPage />} />
+                  </>
+                }
+                {userRole === 'admin' &&
+                  <>
+                    <Route path="/" element={<h1 style={{ marginTop: '10px' }}>Hi Admin!</h1>} />
+                  </>
+                }
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </Layout>
+          ) : (
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/raspored" element={<MainPage />} />
+              <Route path='/' element={<LoginPage setToken={setToken} setUserRole={setUserRole} />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
-          </Layout>
+          )}
+          
         </Router>
-      ) : (
-        <Segment>
-          <Header as="h2" textAlign="center">Login</Header>
-          <Form>
-            <Grid>
-              <Grid.Row columns={2}>
-                <Grid.Column>
-                  <Input
-                    fluid
-                    placeholder="Enter username or email"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </Grid.Column>
-                <Grid.Column>
-                  <Input
-                    fluid
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                  />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column width={16}>
-                  <Button
-                    color='teal'
-                    fluid
-                    onClick={handleLogin}
-                  >
-                    Log in
-                  </Button>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Form>
-        </Segment>
-      )}
     </Container>
   );
 }
